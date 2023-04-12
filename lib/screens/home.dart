26b9c81c1df17534e32ddbfddd729fba870c2428
leadpro456 <ravinduyasiska@gmail.com>
2023-04-12 12:41:33 +0530

    popup at new year

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -79,7 +79,8 @@ class _HomeState extends State<Home> {
 
   TimerCountdown timercountdown() {
     DateTime now = DateTime.now();
-    DateTime endTime = DateTime(now.year, now.month, 14, 14, 59, 0);
+
+    DateTime endTime = DateTime(now.year, now.month, 12, 12, 41, 0);
     Duration difference = endTime.difference(now);
 
     return TimerCountdown(
@@ -104,7 +105,57 @@ class _HomeState extends State<Home> {
       minutesDescription: 'ñks;a;=',
       secondsDescription: ';;amr',
       onEnd: () {
-        print("Timer finished");
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              backgroundColor: Color.fromARGB(255, 145, 15, 3),
+              content: Container(
+                height: 400.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 20),
+                      child: Image(
+                        image: AssetImage('assets/images/showDiallogImage.jpg'),
+                      ),
+                    ),
+                    Text(
+                      "Tn ieug idufhka i;=áka imsß iqn w¨;a wjqreoaola  fõjd æ",
+                      style: TextStyle(
+                        fontFamily: 'SARA-PUNCHI',
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              "Close",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
       },
     );
   }
