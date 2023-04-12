9793114183466486d6b64c69a0c2b04d2826b0c6
leadpro456 <ravinduyasiska@gmail.com>
2023-04-12 09:56:22 +0530

    countdown to new year start in the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../screens/second_page.dart';
+import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
+import 'package:flutter/cupertino.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,64 +14,117 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _buildAppBar(),
-        backgroundColor: Colors.white,
-        body: Container(
-          child: Column(children: [
-            Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0, color: Colors.transparent),
-                ),
-                child: Image.asset('assets/images/image_1.jpg')),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5),
-              child: SizedBox(
-                width: 180,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const SecondPage();
-                        },
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 172, 18, 4),
+      appBar: _buildAppBar(),
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          children: [
+            Stack(
+              //crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0, color: Colors.transparent),
+                    ),
+                    child: Image.asset('assets/images/image_1.jpg'),
                   ),
-                  child: Text('msúfikak',
-                      style: TextStyle(
-                        fontFamily: 'SARA-PUNCHI',
-                        fontSize: 20,
-                      )),
                 ),
-              ),
+                Positioned(
+                  bottom: 20,
+                  right: 25,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      child: SizedBox(
+                        width: 125,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return const SecondPage();
+                                },
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 172, 18, 4),
+                          ),
+                          child: Text(
+                            'msúfikak',
+                            style: TextStyle(
+                              fontFamily: 'SARA-PUNCHI',
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ]),
-        ));
-  }
-}
-
-AppBar _buildAppBar() {
-  return AppBar(
-    backgroundColor: Color.fromARGB(255, 172, 18, 4),
-    elevation: 0,
-    title: Center(
-      child: Text(
-        'isxy, yd oñ< w¨;a wjqreoao - 2023',
-        style: TextStyle(
-          fontFamily: 'Astro11',
-          fontSize: 28,
-          height: 1.2,
-          fontWeight: FontWeight.w500,
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: timercountdown(),
+            ),
+          ],
         ),
-        textAlign: TextAlign.center,
       ),
-    ),
-  );
+    );
+  }
+
+  TimerCountdown timercountdown() {
+    DateTime now = DateTime.now();
+    DateTime endTime = DateTime(now.year, now.month, 14, 14, 59, 0);
+    Duration difference = endTime.difference(now);
+
+    return TimerCountdown(
+      colonsTextStyle: const TextStyle(
+        color: Color.fromARGB(255, 19, 102, 204),
+        fontWeight: FontWeight.w400,
+        fontSize: 20,
+      ),
+      format: CountDownTimerFormat.daysHoursMinutesSeconds,
+      endTime: now.add(difference),
+      timeTextStyle: TextStyle(
+        color: Color.fromARGB(255, 19, 102, 204),
+        fontWeight: FontWeight.w500,
+        fontSize: 24,
+      ),
+      descriptionTextStyle: TextStyle(
+        fontFamily: 'SARA-PUNCHI',
+        fontSize: 18,
+      ),
+      daysDescription: 'Èk',
+      hoursDescription: 'meh',
+      minutesDescription: 'ñks;a;=',
+      secondsDescription: ';;amr',
+      onEnd: () {
+        print("Timer finished");
+      },
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Color.fromARGB(255, 172, 18, 4),
+      elevation: 0,
+      title: Center(
+        child: Text(
+          'isxy, yd oñ< w¨;a wjqreoao - 2023',
+          style: TextStyle(
+            fontFamily: 'Astro11',
+            fontSize: 28,
+            height: 1.2,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
 }
